8d02308c0d231dcbdfda7c1065ee4c17ea3bb79f
Erik Malo Schuessler <90277878+Brainspeck@users.noreply.github.com>
2025-11-16 20:02:56 +0000

    created the card and added styles

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  const getRandomValue = () => {
+    const values = ["A", "J", "Q", "K", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+     
+    let randomValue = values[Math.floor(Math.random() * values.length)];
+    
+    console.log(randomValue);
+    return randomValue
+  }
+
+  getRandomValue();
 };
