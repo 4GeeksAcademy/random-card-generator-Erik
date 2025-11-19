5bcd0debf1b51faa9b6e7c2ce6bd11f8698bc114
Erik Malo Schuessler <90277878+Brainspeck@users.noreply.github.com>
2025-11-19 03:16:26 +0000

    added functions for randomize suits and values

--- src/app.js
+++ src/app.js
@@ -6,14 +6,53 @@ import "./style.css";
 window.onload = function() {
   //write your code here
   
-  const getRandomValue = () => {
+  getRandomCard();
+};
+
+
+
+const getRandomCard = () => {
     const values = ["A", "J", "Q", "K", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
      
     let randomValue = values[Math.floor(Math.random() * values.length)];
+
+    const cardValue = document.getElementById("value");
+
+    cardValue.innerHTML = randomValue;
+
+    const suits = ["♦", "♥", "♠", "♣"];
+
+    let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+
+    const upperSuit = document.getElementById("upper");
+    const lowerSuit = document.getElementById("lower");
+
+    upperSuit.innerHTML = randomSuit;
+    lowerSuit.innerHTML = randomSuit;
+
+    if (randomSuit === '♦' || randomSuit === '♥') {
+      upperSuit.style.color = 'red';
+      lowerSuit.style.color = 'red';
+      cardValue.style.color = 'red';
+    }
+
+    if (randomSuit === '♠' || randomSuit === '♣') {
+      upperSuit.style.color = 'black';
+      lowerSuit.style.color = 'black';
+      cardValue.style.color = 'black';
+    }
+
     
-    console.log(randomValue);
-    return randomValue
+    console.log(randomValue, randomSuit);
+    return randomValue;
   }
 
-  getRandomValue();
-};
+  const newCardBtn = document.getElementById("new-card")
+
+  newCardBtn.addEventListener('click', () => {
+    getRandomCard();
+  })
+
+  setInterval(() => {
+    getRandomCard();
+  },10000)
